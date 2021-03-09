c65c4c25b5f2fbd17060969ace121149d2378adb
Alexdrowned <axel_art_Bautista@outlook.com>
2021-03-08 19:10:14 -0600

    Nuevos archivos

---
+++ p.py
@@ -0,0 +1,8 @@
+def main():
+    print('Esto no es mas que un hola mundo en Python')
+    print('1 + 1 = 11 Esto es una cadena')
+    a = 1 ; b = 1; c = a+b
+    print('La suma de 1 + 1 es: ',c)
+
+
+main()
